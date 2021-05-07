99b97435f4dd1dcbc6bf56ecd0c49ac023c2364c
Eduardo <eduardo.benigno15@gmail.com>
2021-05-07 08:45:33 -0300

    preparando para dia 3

---
+++ react/components/helloWorld/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./global.css"
+
+const helloWorld = () => {
+  return (
+    <>
+      <p className="nombre-de-la-clase">Eso es un componente customizado!</p>
+    </>
+  );
+};
+
+export default helloWorld;
+
+
---
+++ react/helloWorld.js
@@ -0,0 +1,4 @@
+import helloWorld from './components/helloWorld/index.js';
+
+export default helloWorld
+
