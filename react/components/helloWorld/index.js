6c0a3b677330019d80cc702386b96f9e6cb1e588
Eduardo <eduardo.benigno15@gmail.com>
2021-05-31 16:20:44 -0300

    dia dois

---
+++ react/components/helloWorld/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const helloWorld = () =>{
+    return(
+        <>
+            <p>VTEX IO Training - Day 2: 25/05/21</p>
+        </>
+    )
+}
+
+export default helloWorld;
